2f0351f393b04f231a28f6219fa6a97ec6f548f6
Guilherme Otávio de Oliveira <guiguioliveira117@gmail.com>
2024-11-21 22:22:06 -0300

    criado a unionfind

---
+++ Quarto Periodo/Grafos/Estudando/UnionFind.hpp
@@ -0,0 +1,39 @@
+#include <iostream>
+#include <vector>
+#include <algorithm>
+
+using namespace std;
+
+class UnionFind {
+private:
+    vector<int> parent, rank;
+public:
+    UnionFind(int n) : parent(n), rank(n, 1) {
+        for (int i = 0; i < n; ++i) {
+            parent[i] = i;
+        }
+    }
+
+    int find(int u) {
+        if (parent[u] != u) {
+            parent[u] = find(parent[u]);
+        }
+        return parent[u];
+    }
+
+    void unionSets(int u, int v) {
+        int rootU = find(u);
+        int rootV = find(v);
+        
+        if (rootU != rootV) {
+            if (rank[rootU] > rank[rootV]) {
+                parent[rootV] = rootU;
+            } else if (rank[rootU] < rank[rootV]) {
+                parent[rootU] = rootV;
+            } else {
+                parent[rootV] = rootU;
+                rank[rootU]++;
+            }
+        }
+    }
+};
